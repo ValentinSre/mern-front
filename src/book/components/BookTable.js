1735322ccd471ba56eede7d0a23efea137fe98b4
Valentin SEREE <XR6354@equans.com>
2023-03-29 21:00:01 +0200

    Add sorting on array + clean columns

--- src/book/components/BookTable.js
+++ src/book/components/BookTable.js
@@ -46,38 +46,97 @@ const BookTable = ({ books: initialBooks, loading: initialLoading }) => {
     setLoading(initialLoading);
   }, [initialBooks, initialLoading]);
 
+  const [sortBy, setSortBy] = useState(null);
+  const [sortOrder, setSortOrder] = useState("asc");
+
+  const handleSort = (columnName) => {
+    if (sortBy === columnName) {
+      // If already sorted by this column, reverse the order
+      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    } else {
+      // Otherwise, sort by this column in ascending order
+      setSortBy(columnName);
+      setSortOrder("asc");
+    }
+  };
+
+  const sortedData = books.sort((a, b) => {
+    if (sortBy) {
+      // If a sort column is selected, sort by that column
+      const aValue =
+        a[sortBy] != null
+          ? typeof a[sortBy] === "string"
+            ? a[sortBy].toLowerCase()
+            : a[sortBy]
+          : "";
+      const bValue =
+        b[sortBy] != null
+          ? typeof b[sortBy] === "string"
+            ? b[sortBy].toLowerCase()
+            : b[sortBy]
+          : "";
+      if (aValue === null && bValue === null) {
+        return 0;
+      } else if (aValue === null) {
+        return sortOrder === "asc" ? 1 : -1;
+      } else if (bValue === null) {
+        return sortOrder === "asc" ? -1 : 1;
+      } else if (aValue < bValue) {
+        return sortOrder === "asc" ? -1 : 1;
+      } else if (aValue > bValue) {
+        return sortOrder === "asc" ? 1 : -1;
+      }
+    }
+    // If no sort column is selected, maintain the original order
+    return 0;
+  });
+
   return (
     <div>
       {!loading && books && (
         <table className='book-table'>
           <thead>
             <tr>
-              <th>Titre</th>
-              <th>Série</th>
-              <th>Tome</th>
-              <th>Auteur</th>
-              <th>Dessinateur</th>
-              <th>Éditeur</th>
-              <th>Format</th>
-              <th>Prix</th>
-              <th>Date de parution</th>
-              <th>Genre</th>
+              <th onClick={() => handleSort("serie")}>
+                Série {sortBy === "serie" && (sortOrder === "asc" ? "⏶" : "⏷")}
+              </th>
+              <th onClick={() => handleSort("titre")}>
+                Titre {sortBy === "titre" && (sortOrder === "asc" ? "⏶" : "⏷")}
+              </th>
+              <th onClick={() => handleSort("tome")}>
+                Tome {sortBy === "tome" && (sortOrder === "asc" ? "⏶" : "⏷")}
+              </th>
+              {/* <th>Auteur</th>
+              <th>Dessinateur</th> */}
+              <th onClick={() => handleSort("editeur")}>
+                Éditeur{" "}
+                {sortBy === "editeur" && (sortOrder === "asc" ? "⏶" : "⏷")}
+              </th>
+              <th onClick={() => handleSort("format")}>
+                Format{" "}
+                {sortBy === "format" && (sortOrder === "asc" ? "⏶" : "⏷")}
+              </th>
+              <th onClick={() => handleSort("prix")}>
+                Prix {sortBy === "prix" && (sortOrder === "asc" ? "⏶" : "⏷")}
+              </th>
+              {/* <th>Date de parution</th>
+              <th>Genre</th> */}
               {auth.isLoggedIn && <th>Actions</th>}
             </tr>
           </thead>
           <tbody>
-            {books.map((book) => (
+            {sortedData.map((book) => (
               <tr key={book._id} onClick={() => handleViewBook(book)}>
-                <td>{book.titre}</td>
                 <td>{book.serie}</td>
+                <td>{book.titre}</td>
                 <td>{book.tome}</td>
-                <td>{book.auteur}</td>
-                <td>{book.dessinateur}</td>
+                {/* <td>{book.auteur}</td>
+                <td>{book.dessinateur}</td> */}
                 <td>{book.editeur}</td>
                 <td>{book.format}</td>
                 <td>{book.prix}€</td>
-                <td>{new Date(book.date_parution).toLocaleDateString()}</td>
-                <td>{book.genre}</td>
+                {/* <td>{new Date(book.date_parution).toLocaleDateString()}</td>
+                <td>{book.genre}</td> */}
                 {auth.isLoggedIn && (
                   <td>
                     <div className='button-group'>
