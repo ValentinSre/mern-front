c2e600c3d10849caa7dc5785161126b0cc851dd1
Valentin <73347253+ValentinSre@users.noreply.github.com>
2023-03-30 15:46:36 +0200

    update including generic table
    not tested, see previous commit

--- src/book/pages/BookList.js
+++ src/book/pages/BookList.js
@@ -3,8 +3,8 @@ import React, { useEffect, useState, useContext } from "react";
 import { useHttpClient } from "../../shared/hooks/http-hook";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import Table from "../../shared/components/UIElements/Table";
 import { AuthContext } from "../../shared/context/auth-context";
-import BookTable from "../components/BookTable";
 
 const BookList = () => {
   const auth = useContext(AuthContext);
@@ -19,11 +19,60 @@ const BookList = () => {
         );
 
         setLoadedBooks(responseData.books);
-      } catch (err) {}
+      } catch (err) { }
     };
 
     fetchBooks();
   }, [sendRequest, auth.userId]);
+
+  const handleViewBook = (bookId) => {
+    console.log(bookId);
+  };
+
+  const columnsName = [
+    { name: "Série", id: "serie", sort: true },
+    { name: "Titre", id: "titre", sort: true },
+    { name: "Tome", id: "tome", sort: true },
+    { name: "Éditeur", id: "editeur", sort: true },
+    { name: "Format", id: "format", sort: true },
+    { name: "Prix", id: "prix", sort: true },
+  ]
+
+  const handleAddToList = async (bookId, listName) => {
+    try {
+      const responseData = await sendRequest(
+        process.env.REACT_APP_API_URL + "/collection/add",
+        "POST",
+        JSON.stringify({
+          id_book: bookId,
+          id_user: auth.userId,
+          list_name: listName,
+          books: loadedBooks,
+        }),
+        {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + auth.token,
+        }
+      );
+
+      setLoadedBooks(responseData.books);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleAddToCollection = (bookId) => {
+    handleAddToList(bookId, 'collection')
+  }
+
+  const handleAddToWishlist = (bookId) => {
+    handleAddToList(bookId, 'wishlist')
+  }
+
+  const actions = [
+    { icon: 'collection', title: 'Ajouter à ma collection', handleAction: handleAddToCollection, disabled: ['possede'] },
+    { icon: 'wishlist', title: 'Ajouter à ma wishlist', handleAction: handleAddToWishlist, disabled: ['possede', 'souhaite'] }
+  ]
 
   return (
     <React.Fragment>
@@ -34,7 +83,7 @@ const BookList = () => {
         </div>
       )}
       {!isLoading && loadedBooks && (
-        <BookTable books={loadedBooks} isLoading={isLoading} />
+        <Table data={loadedBooks} loading={isLoading} onRowClick={handleViewBook} columns={columnsName} actions={actions} />
       )}
     </React.Fragment>
   );
