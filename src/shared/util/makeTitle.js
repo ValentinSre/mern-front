4e298d7cda2d9a10e045237afd41feb5ef460b0f
Valentin SEREE <XR6354@equans.com>
2023-04-09 15:16:10 +0200

    improve makeTitle

--- src/shared/util/makeTitle.js
+++ src/shared/util/makeTitle.js
@@ -1,13 +1,9 @@
 export default function makeTitle(book) {
   return book.serie
     ? book.version
-      ? book.serie +
-        " (v" +
-        book.version +
-        ") T" +
-        book.tome +
-        " - " +
-        book.titre
+      ? book.serie + " (v" + book.version + ") " + book.tome
+        ? "T" + book.tome + " - " + book.titre
+        : " - " + book.titre
       : book.serie + " T" + book.tome + " - " + book.titre
     : book.titre;
 }
