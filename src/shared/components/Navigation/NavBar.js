4aba26be6136e1bf8a58c03135b600d5bc7e2e13
Valentin SEREE <XR6354@equans.com>
2023-04-14 19:28:56 +0200

    new navbar!

---
+++ src/shared/components/Navigation/NavBar.js
@@ -0,0 +1,168 @@
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Menu,
+  MenuItem,
+  TextField,
+  Paper,
+  InputBase,
+  Divider,
+  Tooltip,
+} from "@material-ui/core";
+import { FaDice } from "react-icons/fa";
+import SearchIcon from "@material-ui/icons/Search";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import { Link } from "react-router-dom";
+
+import { AuthContext } from "../../context/auth-context";
+import logo from "../../images/logo.png";
+
+import StaticNavBar from "./StaticNavBar";
+
+import "./NavBar.css";
+
+const NavBar = () => {
+  const history = useHistory();
+  const auth = useContext(AuthContext);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const renderMenuItems = () => {
+    return (
+      <div className='menu-items'>
+        {auth.isLoggedIn && (
+          <React.Fragment>
+            <MenuItem>
+              <NavLink to={`/${auth.userId}/collection`} className='menu-link'>
+                Ma collection
+              </NavLink>
+            </MenuItem>
+            <MenuItem>
+              <NavLink to={`/${auth.userId}/wishlist`} className='menu-link'>
+                Ma wishlist
+              </NavLink>
+            </MenuItem>
+            <MenuItem>
+              <NavLink to={`/${auth.userId}/stats`} className='menu-link'>
+                Mes statistiques
+              </NavLink>
+            </MenuItem>
+          </React.Fragment>
+        )}
+        <MenuItem>
+          <NavLink to={`/books`} className='menu-link'>
+            La bibliothèque
+          </NavLink>
+        </MenuItem>
+        {auth.isAdmin && (
+          <MenuItem>
+            <NavLink to='/book/new' className='menu-link'>
+              Ajouter un livre
+            </NavLink>
+          </MenuItem>
+        )}
+      </div>
+    );
+  };
+
+  return (
+    <React.Fragment>
+      <AppBar position='fixed' style={{ backgroundColor: "#ffde59" }}>
+        <Toolbar
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            flexGrow: 1,
+          }}
+        >
+          <img
+            src={logo}
+            alt='WebVerse'
+            className='logo'
+            onClick={() => history.push("/")}
+          />
+
+          <div className='searchBarWrapper'>
+            <Paper component='form' className='searchBar'>
+              <IconButton type='button' sx={{ p: "10px" }} aria-label='search'>
+                <SearchIcon />
+              </IconButton>
+              <InputBase
+                style={{ marginLeft: "8px", flex: 1 }}
+                placeholder='Rechercher un album, une série, un auteur...'
+                inputProps={{ "aria-label": "search google maps" }}
+              />
+            </Paper>
+          </div>
+
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <Link to='/suggestion'>
+              <Tooltip title='Suggérer un contenu'>
+                <IconButton edge='end' color='inherit'>
+                  <FaDice style={{ color: "#fff" }} />
+                </IconButton>
+              </Tooltip>
+            </Link>
+            <IconButton
+              edge='end'
+              aria-label='account of current user'
+              aria-controls='menu-appbar'
+              aria-haspopup='true'
+              onClick={handleMenuOpen}
+              color='inherit'
+            >
+              <AccountCircle />
+            </IconButton>
+          </div>
+          <Menu
+            id='menu-appbar'
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+            style={{ marginTop: "40px" }}
+          >
+            {renderMenuItems()}
+            <Divider />
+            {!auth.isLoggedIn && (
+              <MenuItem>
+                <NavLink to='/auth' className='menu-link'>
+                  Se connecter
+                </NavLink>
+              </MenuItem>
+            )}
+            <MenuItem>
+              <div onClick={auth.logout} className='menu-link'>
+                Déconnexion
+              </div>
+            </MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+      <StaticNavBar userId={auth.userId} />
+    </React.Fragment>
+  );
+};
+
+export default NavBar;
---
+++ src/shared/components/Navigation/StaticNavBar.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import { Typography, IconButton, Tooltip } from "@material-ui/core";
+import { BsFillCaretDownFill } from "react-icons/bs";
+
+import "./StaticNavBar.css";
+
+const StaticNavBar = ({ userId }) => {
+  const categories = ["BD", "Comics", "Manga"];
+
+  return (
+    <div className='static-nav-bar'>
+      <div style={{ marginLeft: "20px", display: "flex" }}>
+        {categories.map((category) => (
+          <Tooltip title={`Cadre ${category}`} key={category}>
+            <div
+              style={{
+                color: "#ffffff",
+                display: "flex",
+                alignItems: "center",
+                marginRight: "20px",
+                position: "relative",
+              }}
+            >
+              <NavLink
+                to={`/category/${category.toLowerCase()}`}
+                style={{
+                  color: "#ffffff",
+                  textDecoration: "none",
+                  marginRight: "5px",
+                }}
+              >
+                {category}
+              </NavLink>
+              <BsFillCaretDownFill
+                style={{
+                  color: "#ffffff",
+                  fontSize: "12px",
+                  marginLeft: "5px",
+                }}
+              />
+            </div>
+          </Tooltip>
+        ))}
+      </div>
+      <div style={{ flexGrow: 1 }}></div>
+      <div style={{ marginRight: "20px" }}>
+        <NavLink
+          to={`/${userId}/collection`}
+          style={{ color: "#ffffff", textDecoration: "none" }}
+        >
+          <Typography variant='button'>Mes BD</Typography>
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default StaticNavBar;
