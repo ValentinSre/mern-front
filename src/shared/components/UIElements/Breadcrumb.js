e8cd2be066b57389e558cd136ffa7316d0bb0989
Valentin S <valentin.seree@sogeti.com>
2023-05-02 15:54:26 +0200

    update breadcrumb

--- src/shared/components/UIElements/Breadcrumb.js
+++ src/shared/components/UIElements/Breadcrumb.js
@@ -1,26 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Breadcrumbs from "@material-ui/core/Breadcrumbs";
 import Link from "@material-ui/core/Link";
 import HomeIcon from "@material-ui/icons/Home";
-import WhatshotIcon from "@material-ui/icons/Whatshot";
-import GrainIcon from "@material-ui/icons/Grain";
+import { ImBooks, ImBook } from "react-icons/im";
+
+import { AuthContext } from "../../context/auth-context";
 
 const useStyles = makeStyles((theme) => ({
   link: {
     display: "flex",
+    fontSize: "10px",
   },
   icon: {
     marginRight: theme.spacing(0.5),
-    width: 20,
-    height: 20,
+    width: 14,
+    height: 14,
   },
 }));
 
 const Breadcrumb = ({ collection, page }) => {
   const history = useHistory();
+  const auth = useContext(AuthContext);
   const classes = useStyles();
 
   const handleClick = (path) => {
@@ -40,15 +43,17 @@ const Breadcrumb = ({ collection, page }) => {
       </Link>
       <Link
         color="inherit"
-        href="/books"
-        onClick={() => handleClick("/books")}
         className={classes.link}
+        onClick={() => {
+          const path = collection ? `/${auth.userId}/collection` : "/books";
+          handleClick(path);
+        }}
       >
-        <WhatshotIcon className={classes.icon} />
+        <ImBooks className={classes.icon} />
         {collection ? "Ma collection" : "La bibliothèque"}
       </Link>
       <Typography color="textPrimary" className={classes.link}>
-        <GrainIcon className={classes.icon} />
+        <ImBook className={classes.icon} />
         {page}
       </Typography>
     </Breadcrumbs>
